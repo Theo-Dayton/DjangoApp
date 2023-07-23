c64aa821eadd10a198716504d8f7bcb6c9218e46
Theo-Dayton <69463213+Theo-Dayton@users.noreply.github.com>
2023-07-23 11:26:25 -0400

    updated models.py

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField()
+    quantity = models.IntegerField(default=0)
+    unit = models.CharField()
+    unit_price = models.IntegerField(default=0)
+    total_price = models.IntegerField(default=0)
+
+class MenuItem(models.Model):
+    name = models.CharField()
+    price = models.IntegerField(default=0)
+    image_link = models.CharField()
+
+class RecipeRequirement(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    ingredient_quantity = models.IntegerField(default=0)
+
+class Purchase(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField()
