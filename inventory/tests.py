06a7708e66240ee8d3ad204c930094b490d33905
Theo-Dayton <69463213+Theo-Dayton@users.noreply.github.com>
2023-07-25 17:36:14 -0400

    unit tests and static files

--- inventory/tests.py
+++ inventory/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from inventory.models import Ingredient
 
-# Create your tests here.
+
+class PostTestCase(TestCase):
+    def testPost(self):
+        post = Ingredient(name="Apple", unit="pound", unit_price=3)
+        self.assertEqual(post.name, "Apple")
+        self.assertEqual(post.unit, "pound")
+        self.assertEqual(post.unit_price, 3)
