8316c1c88f6b5c02e92d1ece7ceeb05f0199ee75
Theo-Dayton <69463213+Theo-Dayton@users.noreply.github.com>
2023-07-25 17:58:43 -0400

    gitignore

--- djangoapp/settings.py
+++ djangoapp/settings.py
@@ -9,19 +9,32 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import json
+from django.core.exceptions import ImproperlyConfigured
 from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+KEYSDIR = str(BASE_DIR)+"/keys.json"
+ 
+with open(KEYSDIR) as k:
+    project_keys = json.loads(k.read())
+ 
+def getKey(setting,project_keys=project_keys):
+    try:
+        return project_keys[setting]
+    except KeyError:
+        errorMessage = "Set the {} env var".format(setting)
+        raise ImproperlyConfigured(errorMessage)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ibru419hz$5(%6y^%fn&6e$+y5!$@+dkalg16xo90mupzm!#a5'
+SECRET_KEY = getKey("SECRETKEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
